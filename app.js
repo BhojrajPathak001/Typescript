851db04813b5dedcbf345135f82e0586b3cb92f7
indeed <bhojrajpathak001@gmail.com>
2024-06-19 01:15:00 +0530

    when we add typescript and when we compile it using typescript compiler it basically does the sanity check like in this code if we try to give n1 as a string it gives use error

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 1;
+var n2 = 2.8;
+console.log(add(n1, n2));
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+const n1 = 1;
+const n2 = 2.8;
+
+console.log(add(n1, n2));
