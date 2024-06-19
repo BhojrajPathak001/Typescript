14c8c6e794c10ea531dac2a695402e47f2a2f551
indeed <bhojrajpathak001@gmail.com>
2024-06-19 09:13:48 +0530

    enum add up g

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ enum Role {
   ADMIN,
   READ_ONLY,
   AUTHOR,
-}
+} //hum khud bhi enums ko value de skte hai but sare same hona chaheye agar default nature change kar rhe hai tou
 
 const person = {
   name: "bhoraj",
