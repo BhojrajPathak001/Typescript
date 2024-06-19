d9293992781c457c1125360110871e874a67fce3
indeed <bhojrajpathak001@gmail.com>
2024-06-19 10:02:08 +0530

    literal types

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
-function combine(input1: number | string, input2: number | string) {
-  if (typeof input1 === "number" && typeof input2 === "number") {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text" //literal types
+) {
+  if (
+    typeof input1 === "number" &&
+    typeof input2 === "number" &&
+    resultConversion === "as-number"
+  ) {
     return input1 + input2;
   } else {
     return input1.toString() + input2.toString();
   }
 }
 
-const age = combine(1, 2);
-const fullName = combine("bhojraj ", "pathak");
+const age = combine(1, 2, "as-number");
+const fullName = combine("bhojraj ", "pathak", "as-text");
 
 console.log(age);
 console.log(fullName);
