38c030ea2e240817d2f61c6da2b71a957c4ae416
indeed <bhojrajpathak001@gmail.com>
2024-06-20 19:24:26 +0530

    never type

--- app.ts
+++ app.ts
@@ -8,3 +8,10 @@ userName = userInput;
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  //it's better than void it tells that function kbhi khtm hoga he ni script bich me ruk jayegi run hons
+  throw { message: MessageChannel, errorCode: code };
+}
+
+generateError("An eror occured", 500);
