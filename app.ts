08cc183fc55fe0d2635bf75dba860c7df556aa45
indeed <bhojrajpathak001@gmail.com>
2024-06-20 19:17:37 +0530

    unknown type

--- app.ts
+++ app.ts
@@ -1,29 +1,10 @@
-function add(n1: number, n2: number) {
-  //return type typescript infer kr leta hai
-  return n1 + n2; //here the
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+userName = userInput;
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log(num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  const n = cb(result);
-  n.toString();
-}
-
-let combineValues: (n1: number, n2: number) => number;
-combineValues = add;
-
-// combineValues = printResult;
-
-// console.log(combineValues(8, 8));
-
-console.log(
-  "main log",
-  addAndHandle(10, 20, (result) => {
-    console.log(result);
-    return result;
-  })
-);
