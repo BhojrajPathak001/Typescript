207b6a7d79de2b1a4c4d5f0962a86bf52de30108
indeed <bhojrajpathak001@gmail.com>
2024-06-23 00:49:40 +0530

    generic functions with constraints

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,12 @@
 "use strict";
 function merge(objA, objB) {
+    //here extends object are constraints that we set
     return Object.assign(objA, objB);
 }
 /*jab hum generics user krte hai aise and hum jab wo generic function call krte hai tou T and U ki
    jagah dynamic type chale jata hai apne aap humko specify nhi krna pdhta  */
 const mergeObj1 = merge({ name: "bhojraj", hobbies: ["sports"] }, { age: 23 });
-const mergeObj2 = merge(//yaha par ese bhi specify kar skte hai but redundant he yeh 
+const mergeObj2 = merge(//yaha par ese bhi specify kar skte hai but redundant he yeh
 { name: "bhojraj" }, { age: 23 });
 console.log(mergeObj1.name); // "bhojraj"
 console.log(mergeObj1.age); // 23
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 function merge<T extends object, U extends object>(objA: T, objB: U) {
+  //here extends object are constraints that we set
   return Object.assign(objA, objB);
 }
 
 /*jab hum generics user krte hai aise and hum jab wo generic function call krte hai tou T and U ki
    jagah dynamic type chale jata hai apne aap humko specify nhi krna pdhta  */
 const mergeObj1 = merge({ name: "bhojraj", hobbies: ["sports"] }, { age: 23 });
-const mergeObj2 = merge<{ name: string }, { age: number }>( //yaha par ese bhi specify kar skte hai but redundant he yeh 
+const mergeObj2 = merge<{ name: string }, { age: number }>( //yaha par ese bhi specify kar skte hai but redundant he yeh
   { name: "bhojraj" },
   { age: 23 }
 );
