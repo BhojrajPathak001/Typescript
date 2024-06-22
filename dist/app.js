3572a8b783b971a682a3b4a0b95f38e9d884db94
indeed <bhojrajpathak001@gmail.com>
2024-06-22 15:20:46 +0530

    index type

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,5 @@
 "use strict";
-//type casting
-const userInputElement = document.getElementById("user-input"); /*here basically getElementByid return HtmlElement type and if if i make userInputElemnt as
-HtmlInputelement type it will give error we want to typecast it before assing to userInputElement so
-we will typeCast it using as keyword */
-userInputElement.value = "Hi there";
+const errorBag = {
+    email: "not a valid email",
+    username: "must start with a capital character!",
+};
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-//type casting
+interface ErrorContainer {
+  /*suppose humne error ke liye ek obejct chaheye jisme hamesa key value pair
+    string ka hone chaheye tou aise define kar skte hai so that it will enforce every error object to be like that 
+    and it is called index type below is the syntax of it
+  */
+  [prop: string]: string;
+}
 
-const userInputElement = document.getElementById(
-  "user-input"
-)! as HTMLInputElement; /*here basically getElementByid return HtmlElement type and if if i make userInputElemnt as 
-HtmlInputelement type it will give error we want to typecast it before assing to userInputElement so 
-we will typeCast it using as keyword */
-userInputElement.value = "Hi there";
+const errorBag: ErrorContainer = {
+  email: "not a valid email",
+  username: "must start with a capital character!",
+};
