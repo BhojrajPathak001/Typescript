482e7bee273d462407d82f9a242f90403715f5fb
indeed <bhojrajpathak001@gmail.com>
2024-06-23 02:53:01 +0530

    keyof

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,8 @@
 "use strict";
-function merge(objA, objB) {
-    //here extends object are constraints that we set
-    return Object.assign(objA, objB);
+function extractAndConvert(obj, key) {
+    return obj[key];
 }
-/*jab hum generics user krte hai aise and hum jab wo generic function call krte hai tou T and U ki
-   jagah dynamic type chale jata hai apne aap humko specify nhi krna pdhta  */
-const mergeObj1 = merge({ name: "bhojraj", hobbies: ["sports"] }, { age: 23 });
-const mergeObj2 = merge(//yaha par ese bhi specify kar skte hai but redundant he yeh
-{ name: "bhojraj" }, { age: 23 });
-function countAndDescribe(element) {
-    let descriptionText = "Got no value";
-    if (element.length === 1) {
-        descriptionText = "got 1 element";
-    }
-    else if (element.length > 1) {
-        descriptionText = "got " + element.length + " elements";
-    }
-    return [element, descriptionText];
-}
+extractAndConvert({ name: "bhojraj" }, "name");
+/*so basically what problem can happen is humn object aur usme koi key pass kar rhe hai but agar wo key exists he nhi
+krti hogi object  me tou for that we use keyof provided by typescript wo check kr leta hai ki koi key exists krti hai ya nhi
+*/
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,9 @@
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  //here extends object are constraints that we set
-  return Object.assign(objA, objB);
+function extractAndConvert<T extends object,U extends keyof T>(obj:T, key:U) {
+  return obj[key];
 }
 
-/*jab hum generics user krte hai aise and hum jab wo generic function call krte hai tou T and U ki
-   jagah dynamic type chale jata hai apne aap humko specify nhi krna pdhta  */
-const mergeObj1 = merge({ name: "bhojraj", hobbies: ["sports"] }, { age: 23 });
-const mergeObj2 = merge<{ name: string }, { age: number }>( //yaha par ese bhi specify kar skte hai but redundant he yeh
-  { name: "bhojraj" },
-  { age: 23 }
-);
+extractAndConvert({ name: "bhojraj" }, "name");
 
-// console.log(mergeObj1.name); // "bhojraj"
-// console.log(mergeObj1.age); // 23
-// console.log(mergeObj1.hobbies); // ['sports']
-
-// console.log(mergeObj2.name); // "bhojraj"
-// console.log(mergeObj2.age); // 23
-// console.log(mergeObj2.hobbies); // ['sports']
-
-// ------------------------------------------------------------------------------------------
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = "Got no value";
-  if (element.length === 1) {
-    descriptionText = "got 1 element";
-  } else if (element.length > 1) {
-    descriptionText = "got " + element.length + " elements";
-  }
-  return [element, descriptionText];
-}
+/*so basically what problem can happen is humn object aur usme koi key pass kar rhe hai but agar wo key exists he nhi
+krti hogi object  me tou for that we use keyof provided by typescript wo check kr leta hai ki koi key exists krti hai ya nhi 
+*/
